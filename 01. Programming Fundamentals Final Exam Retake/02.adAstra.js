692828039cee17bdc3bffe3daec3272d27f78e75
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2022-08-06 12:56:30 +0300

    Code refactoring.

--- 01. Programming Fundamentals Final Exam Retake/02.adAstra.js
+++ 01. Programming Fundamentals Final Exam Retake/02.adAstra.js
@@ -3,17 +3,20 @@ function adAstra(input) {
     let dailyCalories = 2000;
     let box = [];
     let pattern = /(#|\|)(?<productName>[A-Z a-z]+)\1(?<date>\d{2}\/\d{2}\/\d{2})\d*\1(?<calories>\d{1,5})\1/g;
-    let foodItem = pattern.exec(input)
+    let foodItem = pattern.exec(input);
 
     while (foodItem) {
-        days += Number(foodItem[4]) / dailyCalories;
-        box.push({product: foodItem[2], date: foodItem[3], calorie: foodItem[4]})
+        days += Number(foodItem.groups.calories) / dailyCalories;
+        box.push({
+            product: foodItem.groups.productName, 
+            date: foodItem.groups.date, 
+            calorie: foodItem.groups.calories});
         foodItem = pattern.exec(input);
     }
     console.log(`You have food to last you for: ${Math.floor(days)} days!`);
-    for (const item of box) {
-        console.log(`Item: ${item.product}, Best before: ${item.date}, Nutrition: ${item.calorie}`);
-    }
+    box.forEach(product => {
+        console.log(`Item: ${product.product}, Best before: ${product.date}, Nutrition: ${product.calorie}`);
+    });
 }
 
 adAstra([
